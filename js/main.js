01af7e323e9e3db3364c7d0bf5b320e614973f9e
Jessica <jecw21@gmail.com>
2018-06-22 01:25:36 -0500

    accessibility tasks

--- js/main.js
+++ js/main.js
@@ -37,6 +37,9 @@ fillNeighborhoodsHTML = (neighborhoods = self.neighborhoods) => {
     option.innerHTML = neighborhood;
     option.value = neighborhood;
     select.append(option);
+    if (document.activeElement === option){
+      option.focus();
+    }
   });
 }
 
@@ -162,13 +165,14 @@ createRestaurantHTML = (restaurant) => {
   const image = document.createElement('img');
   image.className = 'restaurant-img';
   image.src = DBHelper.imageUrlForRestaurant(restaurant);
-  image.srcset = DBHelper.srcsetForRestaurant(restaurant); 
+  image.srcset = DBHelper.srcsetForRestaurant(restaurant);
   image.sizes = DBHelper.sizesForRestaurantImg(restaurant);
   image.alt = DBHelper.altForRestaurantImg(restaurant);
   li.append(image);
 
-  const name = document.createElement('h1');
+  const name = document.createElement('a');
   name.innerHTML = restaurant.name;
+  name.href = DBHelper.urlForRestaurant(restaurant);
   li.append(name);
 
   const neighborhood = document.createElement('p');
@@ -179,10 +183,12 @@ createRestaurantHTML = (restaurant) => {
   address.innerHTML = restaurant.address;
   li.append(address);
 
+  /*
   const more = document.createElement('a');
-  more.innerHTML = 'View Details';
+  more.innerHTML = 'View Details on ' + restaurant.name + ' Restaurant';
   more.href = DBHelper.urlForRestaurant(restaurant);
   li.append(more)
+  */
 
   return li
 }
